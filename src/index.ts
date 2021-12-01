4bbcadbc9235a2dbb9dee05916a6ef91cc2be612
Nikita Trotner <nikita@ttnr.me>
2021-12-01 18:31:33 +0100

    added compiled files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import { registerConvert, registerEth, registerWeb3 } from "./compiled/Web3";
 import { Fluence } from "@fluencelabs/fluence";
 import { krasnodar } from "@fluencelabs/fluence-network-environment";
-import { Web3, Eth, Convert } from "./components/services";
 import { createConnection } from "./components/web3";
 import { settings } from "../settings";
+import { Convert } from "./components/implementations/Convert";
+import { Eth } from "./components/implementations/Eth";
+import { Web3 } from "./components/implementations/Web3";
+import { registerConvert } from "./compiled/Convert";
+import { registerEth } from "./compiled/Eth";
+import { registerWeb3 } from "./compiled/Web3";
 
 
 async function main() {
@@ -13,13 +17,14 @@ async function main() {
   
   let web3 = createConnection()
   
+  registerConvert(new Convert());
   registerWeb3(new Web3(web3));
   registerEth(new Eth(web3));
-  registerConvert(new Convert());
   
   console.log("Services Wrapper Application Started");
   console.log("Peer ID: ", Fluence.getStatus().peerId);
   console.log("Relay: ", Fluence.getStatus().relayPeerId);
+  console.log("Connected: ", Fluence.getStatus().isConnected)
 }
 
 main();
